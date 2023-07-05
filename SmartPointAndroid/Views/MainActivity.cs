e7c98dbcaaaa4ac1e465ac98cc65214a2f35ed37
kana2022 <kkanat94@gmail.com>
2023-07-05 10:50:48 +0600

    Add project files.

---
+++ SmartPointAndroid/Views/MainActivity.cs
@@ -0,0 +1,14 @@
+
+namespace SmartPointAndroid.Views;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : AppCompatActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+    }
+}
